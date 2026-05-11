9e68ea8d1bb9cae48164c37b11c5a145fe2fded3
Ashham Shingeri <ashhamshingeri17@gmail.com>
2026-05-11 20:30:46 +0530

    Creating my-app with React JS

---
+++ my-app/src/App.tsx
@@ -0,0 +1,12 @@
+import Courses from "./Courses";
+
+function App() {
+  return (
+    <div>
+      <h1>React JS</h1>
+      <Courses></Courses>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Courses.tsx
@@ -0,0 +1,15 @@
+function Courses() {
+  return (
+    <div>
+      <h3>Course Lists:</h3>
+      <ul>
+        <li>Full Stack - MERN</li>
+        <li>Advance Web Designing</li>
+        <li>Python</li>
+        <li>Artificial Intelligence with Machine Learning</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Courses;
